a06803186455b71dbfadb8a8aa25e15627f77b66
manununhez <manuel.nunhez90@gmail.com>
2021-02-17 18:02:40 +0000

    Bug Fix config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   apps : [{
         name: 'dutch-auction',
-        script: './server-pj-auction.js',
+        script: 'server-pj-auction.js',
         watch: true,
         env_production: {
-            "PORT": 5001,
             "NODE_ENV": "production",
         }
   }],
